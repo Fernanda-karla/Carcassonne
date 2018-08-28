4c5485dfea18c58b82d18deba9971665013f6463
Wanessa Paiva <wanessa.silva@dce.ufpb.br>
2018-08-28 00:38:32 -0300

    Teste passando: iniciarPartidaInvalida

--- src/main/java/br/ufpb/dcx/aps/carcassone/Jogo.java
+++ src/main/java/br/ufpb/dcx/aps/carcassone/Jogo.java
@@ -3,6 +3,21 @@ package br.ufpb.dcx.aps.carcassone;
 public class Jogo {
 
 	public Partida criarPartida(BolsaDeTiles tiles, Cor... sequencia) {
+		if(tiles == null) {
+			throw new ExcecaoJogo("A bolsa de tiles deve ser fornecida na criação de uma partida");
+		}
+		
+		if(sequencia.length < 2) {
+			throw new ExcecaoJogo("Cada partida deve ter uma sequência de pelo menos dois jogadores");
+		}
+		
+		for(int k =0; k < (sequencia.length -1); ++k) {
+			for(int x = k+1; x < sequencia.length; ++x) {
+				if(sequencia[k] == sequencia[x]) {
+					throw new ExcecaoJogo("Não pode haver repetição de cores na sequência de jogadores");
+				}
+			}
+		}
 		return new Partida(tiles);
 	}
 
