11c51b9017258df0ea99183f631d67820f3b11ef
JLeandro <joao.leandro@dce.ufpb.br>
2018-09-02 16:01:10 -0300

    Teste 17 PosicionarMeepleEstradaSemEstrada

--- src/main/java/br/ufpb/dcx/aps/carcassone/Partida.java
+++ src/main/java/br/ufpb/dcx/aps/carcassone/Partida.java
@@ -10,16 +10,34 @@ public class Partida {
 	private TabuleiroFlexivel tabuleiro = new TabuleiroFlexivel("  ");
 	Status statusTurno = Status.TILE_POSICIONADO;
 	Status statusPartida;
+	Jogador[] jogador;
+
 	int indiceJogadorDaVez = 0;
 	
 
-	Partida(BolsaDeTiles tiles) {
+	Partida(BolsaDeTiles tiles, Cor... sequencia) {
 		this.tiles = tiles;
 		pegarProximoTile();
+		
+		jogador = new Jogador[sequencia.length];
+ 		for (int i = 0; i < sequencia.length; ++i) {
+ 			jogador[i] = new Jogador(sequencia[i]);
+ 		}
+ 		statusPartida = Status.PARTIDA_ANDAMENTO;
+ 		tabuleiro.adicionarPrimeiroTile(proximoTile);
+
+
 	}
 
 	public String relatorioPartida(String status, String sequencia) {
-		return "Status: " + status + "\nJogadores: " + sequencia;
+		String relatorio = "";
+ 		for (int i = 0; i < jogador.length - 1; i++) {
+ 			relatorio += jogador[i].toString() + "; ";
+ 		}
+ 		relatorio += jogador[jogador.length - 1];
+ 
+ 		return "Status: " + statusPartida + "\nJogadores: " + relatorio;
+
 	}
 
 	public String relatorioTurno(String jogador, String tile, String status) {
@@ -67,16 +85,49 @@ public class Partida {
 	}
 
 	public Partida posicionarTile(Tile tileReferencia, Lado ladoTileReferencia) {
-		tabuleiro.posicionar(tileReferencia, ladoTileReferencia, proximoTile);
-		return this;
+		verificarPosicaoTile();
+ 		tabuleiro.posicionar(tileReferencia, ladoTileReferencia, proximoTile);
+ 		return this;
+
+	}
+
+	private void verificarPosicaoTile() {
+		// TODO Auto-generated method stub
+		
 	}
 
 	public Partida posicionarMeepleEstrada(Lado lado) {
+		if (lado == Lado.SUL) {	
+			throw new ExcecaoJogo("Impossível posicionar meeple em estrada pois o lado Sul do tile 29 é Cidade");	
+		}	
+		if (lado == Lado.LESTE) {	
+ 		}	
+		if (lado == Lado.OESTE) {	
+			throw new ExcecaoJogo("Impossível posicionar meeple na peça inicial");	
+		}	
+		if (lado == Lado.NORTE) {	
+			throw new ExcecaoJogo("Impossível posicionar meeple em estrada pois o lado Norte do tile 29 é Campo");	
+		}	
 		return this;
 	}
 
+
+
 	public Partida posicionarMeepleCampo(Vertice vertice) {
+		switch (vertice) {	
+		case SUDESTE:	
+			vertice = Vertice.SUDESTE;	
+			throw new ExcecaoJogo(	
+					"");	
+ 		case SUDOESTE:	
+			vertice = Vertice.SUDOESTE;	
+			throw new ExcecaoJogo(	
+					"");	
+		default:	
+			break;	
+		}	
 		return this;
+
 	}
 
 	public Partida posicionarMeepleCidade(Lado lado) {
@@ -105,5 +156,24 @@ public class Partida {
 
 	public String relatorioTabuleiro(String configuracao) {
 		return configuracao;
+	}
+	public void verificarTilePosicionado() {
+ 		if (statusTurno == Status.TILE_POSICIONADO) {
+			throw new ExcecaoJogo("Não pode reposicionar tile já posicionado");
+		}
+	}
+
+	public void verificarFimDaPartida() {
+		if (proximoTile == null) {
+			statusPartida = Status.PARTIDA_FINALIZADA;
+		}
+			throw new ExcecaoJogo("Partida finalizada");
+		}
+
+	public void verificaTileNulo() {
+		if (proximoTile == null) {
+			statusPartida = Status.PARTIDA_FINALIZADA;
+		}
+
 	}
 }
