24f8268cf539fbb56467a91c12250c348235c588
Andrea <andrea.silva@dce.ufpb.br>
2018-09-03 23:43:51 -0300

    função para a classe Jogador

--- src/main/java/br/ufpb/dcx/aps/carcassone/Jogador.java
+++ src/main/java/br/ufpb/dcx/aps/carcassone/Jogador.java
@@ -1,13 +1,43 @@
 package br.ufpb.dcx.aps.carcassone;
 
 public class Jogador {
-
-	public Jogador(Cor cor) {
-		// TODO Auto-generated constructor stub
+	private Cor corJogador;
+	private int pontosDoJogador = 0;
+	private int meeplesDoJogador = 7;
+	
+	public Jogador(Cor corJogador) {
+		this(corJogador, 0, 7);
 	}
-	public String getCor() {
-		// TODO Auto-generated method stub
-		return null;
+
+	public Jogador(Cor corJogador, Integer pontosDoJogador, Integer meeplesDoJogador) {
+		this.corJogador = corJogador;
+		this.pontosDoJogador = pontosDoJogador;
+		this.meeplesDoJogador = meeplesDoJogador;
+		
+	}
+	public Cor getCor() {
+		return corJogador;
+	}
+	
+	public int getPontos() {
+		return pontosDoJogador;
+	}
+	
+	public int quantidadeMeeples() {
+		return meeplesDoJogador;
+	}
+	
+	public void tirarMeeple() {
+		--meeplesDoJogador;
+	}
+	
+	public void reset() {
+		 meeplesDoJogador = 7;
+		 pontosDoJogador = 0;
+	}
+	
+	public String toString() {
+		return corJogador + "(" + pontosDoJogador + ", " + meeplesDoJogador + ")";
 	}
 
 
